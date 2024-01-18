317ed9d2868525ca95dc471a0f75eef2df384db4
Seanm07 <sean@i6.com>
2024-01-18 12:10:37 +0000

    iOS plugin updated with improved share dialog callback support / removed unnessesary MonoHttp folder

--- iOS/PicklePlugin.h
+++ iOS/PicklePlugin.h
@@ -1,28 +1,37 @@
 #import "UnityAppController.h"
 
-@interface PicklePlugin : UIViewController
+@interface PicklePopupDelegate : UIViewController <UIPopoverPresentationControllerDelegate>
+@end
 
-#ifdef __cplusplus
-// If c++ use "C linkage" to avoid name mangling issues 
-extern "C" {
-#endif
-    
-	typedef void (*AppTrackingTransparencyCallback)(unsigned int result);
-	
-    // Display a share prompt in a centered popover window
-    void ShowShare(char* message, char* subject, bool* doShareIcon);
-	
-	// Returns an integer for the tracking auth status, 0 = UNKNOWN, 1 = RESTRICTED, 2 = DENIED, 3 = AUTHORISED
-	int GetTrackingAuthStatus();
-	
-	// Send a request to show the app tracking transparency prompt
-	void RequestTrackingAuth(AppTrackingTransparencyCallback callback);
-	
-	// Returns the URL to open the settings app
-	char* GetSettingsURL();
-	
-#ifdef __cplusplus
-}
-#endif
+@interface PicklePlugin : UIViewController <UIPopoverPresentationControllerDelegate>
+
+    @property (nonatomic, strong) PicklePopupDelegate *popoverDelegate;
+    @property (nonatomic, strong) PicklePlugin *sharedInstance;
+
+    #ifdef __cplusplus
+    // If c++ use "C linkage" to avoid name mangling issues
+    extern "C" {
+    #endif
+        
+        typedef void (*AppTrackingTransparencyCallback)(unsigned int result);
+        
+        // Display a share prompt in a centered popover window
+        void ShowShare(char* message, char* subject, bool* doShareIcon);
+        
+        // Returns an integer for the tracking auth status, 0 = UNKNOWN, 1 = RESTRICTED, 2 = DENIED, 3 = AUTHORISED
+        int GetTrackingAuthStatus();
+        
+        // Send a request to show the app tracking transparency prompt
+        void RequestTrackingAuth(AppTrackingTransparencyCallback callback);
+        
+        // Returns the URL to open the settings app
+        char* GetSettingsURL();
+        
+        // Haptic feedback
+        void TriggerHapticFeedback(char* style, double duration);
+        
+    #ifdef __cplusplus
+    }
+    #endif
 
 @end
--- iOS/PicklePlugin.mm
+++ iOS/PicklePlugin.mm
@@ -1,12 +1,11 @@
 #include "PicklePlugin.h"
 
-// Shared 
 #import <AdSupport/AdSupport.h>
 
-#ifdef __IPHONE_14_0
-	// iOS 14 app tracking transparency
-	#import <AppTrackingTransparency/ATTrackingManager.h>
-	#import <StoreKit/SKAdNetwork.h>
+#if defined(__IPHONE_14_0) || defined(__MAC_11_0)
+    // iOS 14 app tracking transparency
+    #import <AppTrackingTransparency/ATTrackingManager.h>
+    #import <StoreKit/SKAdNetwork.h>
 #endif
 
 #define TRACKING_NOT_DETERMINED 0
@@ -14,139 +13,195 @@
 #define TRACKING_DENIED 2
 #define TRACKING_AUTHED 3
 
-// "+" methods are pretty much static methods (not really but basically)
-// "-" methods are the opposite
+@implementation PicklePopupDelegate
 
-// This can probably be removed, just not sure if the implementation is required even if it's blank
-@implementation PicklePlugin {
-}
-
-extern UIViewController* UnityGetGLViewController();
-
-+(PicklePlugin*) shareMessage:(char*)message shareSubject:(char*)subject shareDoShareIcon:(bool*)doShareIcon{
-    return [[PicklePlugin alloc] initShareMessage:message initShareSubject:subject initDoShareIcon:doShareIcon];
-}
-
--(PicklePlugin*) initShareMessage:(char*)message initShareSubject:(char*)subject initDoShareIcon:(bool*)doShareIcon{
-    // Set self to the reference of the original caller
-    self = [super init];
-    
-    // Create a new array named items
-    NSMutableArray *items = [NSMutableArray new];
-    
-    if(strlen(message) > 0){
-        // Add the message to the item array
-        [items addObject:[NSString stringWithUTF8String:message]];
+#if defined(__IPHONE_13_0)
+    // New popover dismiss handler, introduced iOS 13
+    - (void)presentationControllerDidDismiss:(UIPresentationController *)presentationController {
+        UnitySendMessage("ShareManager", "OnSharePromptClosedIOS", "");
     }
-    
-	if(doShareIcon){
-		UIImage *appIcon = [UIImage imageNamed: [[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIconFiles"] objectAtIndex:0]];
-		
-		if(appIcon != nil)
-			[items addObject:appIcon];
-	}
-	
-    /*if(strlen(imagePath) > 0){
-        // Convert the imagePath into a UTF8 safe path
-        NSString *filePath = [NSString stringWithUTF8String:imagePath];
+#else
+    // Old popover dismiss handler, introduced iOS 8 and depricated iOS 13
+    - (void)popoverPresentationControllerDidDismissPopover:(UIPopoverPresentationController *)popoverPresentationController {
+        UnitySendMessage("ShareManager", "OnSharePromptClosedIOS", "");
+    }
+#endif
+
+    // Called as soon as the popover has been requested to be displayed
+    /*- (void)prepareForPopoverPresentation:(UIPopoverPresentationController *)popoverPresentationController {
         
-        // Try get the contents of the file and store it as a UIImage type
-        UIImage *image = [UIImage imageWithContentsOfFile:filePath];
-        
-        if(image != nil){
-            // Attach the image
-            [items addObject:image];
-        } else {
-            // The image path couldn't be resolved to a UIImage, just add the raw file path to the items instead
-            [items addObject:[NSURL fileURLWithPath:filePath]];
-        }
     }*/
-    
-    // Initialize a new activity https://developer.apple.com/documentation/uikit/uiactivityviewcontroller/1622019-initwithactivityitems
-    UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:nil];
-    
-    if(strlen(subject) > 0){
-        // Set the value of a key name "subject" to the subject parameter we sent on the activity we created
-        [activity setValue:[NSString stringWithUTF8String:subject] forKey:@"subject"];
+
+    // Called when the popover is fully displayed and transition has completed
+    - (void)popoverPresentationCompleted {
+        UnitySendMessage("ShareManager", "OnSharePromptOpenedIOS", "");
     }
-    
-	UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
-	
-	// While the rootViewController has a parent set it to be the parent until it eventually no longer has a parent
-	// https://pinkstone.co.uk/how-to-avoid-whose-view-is-not-in-the-window-hierarchy-error-when-presenting-a-uiviewcontroller/
-	while(rootViewController.presentedViewController)
-		rootViewController = rootViewController.presentedViewController;    
-    
-	
-    // Present the view controller using the popover style
-    activity.modalPresentationStyle = UIModalPresentationPopover;
-    [rootViewController presentViewController:activity animated:YES completion:nil];
-    
-    // Get the popover presentation controller and configure it
-    UIPopoverPresentationController *presentationController = [activity popoverPresentationController];
-    presentationController.permittedArrowDirections = 0;
-    presentationController.sourceView = rootViewController.view;
-    presentationController.sourceRect = CGRectMake(CGRectGetMidX(rootViewController.view.bounds), CGRectGetMidY(rootViewController.view.bounds), 0, 0);;
-	
-    
-    return self;
-}
 
-# pragma mark - C API
-
-void ShowShare(char* message, char* subject, bool* doShareIcon){
-    [PicklePlugin shareMessage:message shareSubject:subject shareDoShareIcon:doShareIcon];
-}
-
-int GetTrackingAuthStatus(){
-	// Are the iOS 14 APIs available?
-    if (@available(iOS 14, *)) {
-        // Don't include any code we don't need though.
-		#ifdef __IPHONE_14_0
-			return (int)ATTrackingManager.trackingAuthorizationStatus;
-		#endif
-    } else {
-        bool trackingEnabled = ASIdentifierManager.sharedManager.isAdvertisingTrackingEnabled;
-        return trackingEnabled ? TRACKING_AUTHED : TRACKING_DENIED;
+    - (void)popoverPresentationDismissDueToCompletion:(bool)completed{
+        UnitySendMessage("ShareManager", "OnSharePromptClosedIOS", "");
     }
-}
 
-void RequestTrackingAuth(AppTrackingTransparencyCallback callback){
-	if (@available(iOS 14, *)) {
-		#ifdef __IPHONE_14_0
-			[ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
-				// 0 - Not determined
-				// 1 - Restricted
-				// 2 - Denied
-				// 3 - Authorized
-				callback((unsigned int) status);
-			}];
-		#endif
-	} else {
-		bool trackingEnabled = ASIdentifierManager.sharedManager.isAdvertisingTrackingEnabled;
-		NSLog(@"RequestTrackingAuth called but iOS 14 API not available, returning %d.\n", trackingEnabled);
-		callback(trackingEnabled ? TRACKING_AUTHED : TRACKING_DENIED);
-	}
-}
+@end
 
-char* GetSettingsURL(){
-	// Grab the string to open the settings app
-	NSURL * url = [NSURL URLWithString: UIApplicationOpenSettingsURLString];
+@implementation PicklePlugin
 
-	// convert to a c string & donesies
-	return ConvertNSStringToCString(url.absoluteString);
-}
+    extern UIViewController* UnityGetGLViewController();
 
-// Convert an NSString to a regular null terminated array of chars
-char* ConvertNSStringToCString( const NSString* nsString ){
-	if (nsString == NULL) return NULL;
+    // Used to keep the reference of the share popup window alive to trigger the callback events
+    static PicklePlugin *sharedInstance = nil;
 
-	const char* nsStringUtf8 = nsString.UTF8String;
-	char* cString = (char*)malloc( strlen(nsStringUtf8) + 1);
-	// strcpy includes the \0 so no need to memset
-	strcpy( cString, nsStringUtf8 );
+    +(PicklePlugin*) sharedInstance {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            sharedInstance = [[self alloc] init];
+        });
+        return sharedInstance;
+    }
 
-	return cString;
-}
+    +(PicklePlugin*) shareMessage:(char*)message shareSubject:(char*)subject shareDoShareIcon:(bool*)doShareIcon{
+        return [[self sharedInstance] initShareMessage:message initShareSubject:subject initDoShareIcon:doShareIcon];
+    }
 
+    -(PicklePlugin*) initShareMessage:(char*)message initShareSubject:(char*)subject initDoShareIcon:(bool*)doShareIcon{
+        // Set self to the reference of the original caller
+        self = [super init];
+        
+        // Create a new array named items
+        NSMutableArray *items = [NSMutableArray new];
+        
+        if(strlen(message) > 0){
+            // Add the message to the item array
+            [items addObject:[NSString stringWithUTF8String:message]];
+        }
+        
+        if(doShareIcon){
+            UIImage *appIcon = [UIImage imageNamed: [[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIconFiles"] objectAtIndex:0]];
+            
+            if(appIcon != nil)
+                [items addObject:appIcon];
+        }
+        
+        // Initialize a new activity https://developer.apple.com/documentation/uikit/uiactivityviewcontroller/1622019-initwithactivityitems
+        UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:nil];
+        
+        if(strlen(subject) > 0){
+            // Set the value of a key name "subject" to the subject parameter we sent on the activity we created
+            [activity setValue:[NSString stringWithUTF8String:subject] forKey:@"subject"];
+        }
+        
+        UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+        
+        // While the rootViewController has a parent set it to be the parent until it eventually no longer has a parent
+        // https://pinkstone.co.uk/how-to-avoid-whose-view-is-not-in-the-window-hierarchy-error-when-presenting-a-uiviewcontroller/
+        while(rootViewController.presentedViewController)
+            rootViewController = rootViewController.presentedViewController;
+        
+        // Create an instance of the PicklePopupDelegate
+        self.popoverDelegate = [[PicklePopupDelegate alloc] init];
+        
+        // Present the view controller using the popover style
+        activity.modalPresentationStyle = UIModalPresentationPopover;
+        
+        // Setup a callback when an event in the share dialog is completed (e.g copy to clipboard, send email)
+        // Note: This will also trigger after selecting to an action which opens another popover window then closing that, e.g share via email then closing email form)
+        activity.completionWithItemsHandler = ^(UIActivityType _Nullable activityType, BOOL completed, NSArray * _Nullable returnedItems, NSError * _Nullable activityError) {
+            [self.popoverDelegate popoverPresentationDismissDueToCompletion:completed];
+        };
+        
+        [rootViewController presentViewController:activity animated:YES completion:^{
+            [self.popoverDelegate popoverPresentationCompleted];
+        }];
+        
+        // Get the popover presentation controller and configure it
+        UIPopoverPresentationController *presentationController = [activity popoverPresentationController];
+        presentationController.permittedArrowDirections = 0;
+        presentationController.sourceView = rootViewController.view;
+        presentationController.sourceRect = CGRectMake(CGRectGetMidX(rootViewController.view.bounds), CGRectGetMidY(rootViewController.view.bounds), 0, 0);
+        
+        // Set the presentationController to delegate callbacks to the PicklePopupDelegate instance we created
+        presentationController.delegate = self.popoverDelegate;
+        
+        return self;
+    }
+
+    - (void)triggerHapticFeedbackWithStyle:(UIImpactFeedbackStyle)style duration:(NSTimeInterval)duration {
+        if (@available(iOS 10.0, *)) {
+            UIImpactFeedbackGenerator *generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:style];
+            [generator prepare];
+            [generator impactOccurred];
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(duration * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [generator prepare];
+            });
+        } else {
+            // We could use AudioServicesPlaySystemSound to vibrate below iOS 10 but our games don't support that anyway so don't bother
+            NSLog(@"[PP] Haptic feedback is not supported on this device.");
+        }
+    }
+
+    # pragma mark - C API
+
+    void ShowShare(char* message, char* subject, bool* doShareIcon){
+        [PicklePlugin shareMessage:message shareSubject:subject shareDoShareIcon:doShareIcon];
+    }
+
+    int GetTrackingAuthStatus(){
+        // Make sure the supported APIs are available
+        if (@available(iOS 14, macOS 11, *)) {
+            return (int)ATTrackingManager.trackingAuthorizationStatus;
+        } else {
+            bool trackingEnabled = ASIdentifierManager.sharedManager.isAdvertisingTrackingEnabled;
+            return trackingEnabled ? TRACKING_AUTHED : TRACKING_DENIED;
+        }
+    }
+
+    void RequestTrackingAuth(AppTrackingTransparencyCallback callback){
+        if (@available(iOS 14, macOS 11, *)) {
+            [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
+                // 0 - Not determined
+                // 1 - Restricted
+                // 2 - Denied
+                // 3 - Authorized
+                callback((unsigned int) status);
+            }];
+        } else {
+            bool trackingEnabled = ASIdentifierManager.sharedManager.isAdvertisingTrackingEnabled;
+            NSLog(@"[PP] RequestTrackingAuth called but the ATTrackingManager API is not available, returning %d.\n", trackingEnabled);
+            callback(trackingEnabled ? TRACKING_AUTHED : TRACKING_DENIED);
+        }
+    }
+
+    char* GetSettingsURL(){
+        // Grab the string to open the settings app
+        NSURL * url = [NSURL URLWithString: UIApplicationOpenSettingsURLString];
+
+        return ConvertNSStringToCString(url.absoluteString);
+    }
+
+    // Convert an NSString to a regular null terminated array of chars
+    char* ConvertNSStringToCString( const NSString* nsString ){
+        if (nsString == NULL) return NULL;
+
+        const char* nsStringUtf8 = nsString.UTF8String;
+        char* cString = (char*)malloc( strlen(nsStringUtf8) + 1);
+        // strcpy includes the \0 so no need to memset
+        strcpy( cString, nsStringUtf8 );
+
+        return cString;
+    }
+
+    void TriggerHapticFeedback(char* style, double duration) {
+        NSString *styleString = [NSString stringWithUTF8String:style];
+        UIImpactFeedbackStyle impactStyle = UIImpactFeedbackStyleLight; // Default style
+        
+        if ([styleString isEqualToString:@"medium"]) {
+            impactStyle = UIImpactFeedbackStyleMedium;
+        } else if ([styleString isEqualToString:@"heavy"]) {
+            impactStyle = UIImpactFeedbackStyleHeavy;
+        }
+        
+        double feedbackDuration = duration > 0 ? duration : 0.1; // Default duration if not provided
+        
+        [[PicklePlugin new] triggerHapticFeedbackWithStyle:impactStyle duration:feedbackDuration];
+    }
 @end
